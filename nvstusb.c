078379144258e193d5d2f2ad7f372341b99c7290
kolrabi <kolrabi@67836e27-f5b4-4964-bed4-e77daa8fa5ec>
2010-05-13 14:05:25 +0000

    - more information

--- nvstusb.c
+++ nvstusb.c
@@ -23,26 +23,23 @@ static PFNGLXGETVIDEOSYNCSGIPROC glXGetVideoSyncSGI = 0;
 static PFNGLXWAITVIDEOSYNCSGIPROC glXWaitVideoSyncSGI = 0;
 static PFNGLXSWAPINTERVALSGIPROC glXSwapInterval = 0;
 
-#define NVSTUSB_CLOCK 24000000LL
-#define NVSTUSB_CONST_ORIGINAL 0
+/* cpu clock */
+#define NVSTUSB_CLOCK           48000000LL
 
-#if NVSTUSB_CONST_ORIGINAL
-/* these create roughly the same values as the original driver does */
-#define NVSTUSB_CONST_X     (0.053520)  /* ? */
-#define NVSTUSB_CONST_PHASE (0.232200)  /* phase */
-#define NVSTUSB_CONST_DARK  (0.286090)  /* off time */
-#define NVSTUSB_CONST_Y     (0.481768)  /* ? */
-#else
-/* these have been tuned to look correct */
-#define NVSTUSB_CONST_X     (0.053520)  /* ? */
-#define NVSTUSB_CONST_PHASE (0.300000)  /* phase */
-#define NVSTUSB_CONST_DARK  (0.298100)  /* off time */
-#define NVSTUSB_CONST_Y     (0.482100)  /* ? */
-#endif
+/* T0 runs at 4 MHz */
+#define NVSTUSB_T0_CLOCK        (NVSTUSB_CLOCK/12LL)
+#define NVSTUSB_T0_COUNT(us)    (-(us)*(NVSTUSB_T0_CLOCK/1000000)+1)
+
+/* T2 runs at 12 MHz */
+#define NVSTUSB_T2_CLOCK        (NVSTUSB_CLOCK/ 4LL)
+#define NVSTUSB_T2_COUNT(us)    (-(us)*(NVSTUSB_T2_CLOCK/1000000)+1)
 
 #define NVSTUSB_CMD_WRITE       (0x01)  /* write data */
 #define NVSTUSB_CMD_READ        (0x02)  /* read data */
 #define NVSTUSB_CMD_CLEAR       (0x40)  /* set data to 0 */
+
+#define NVSTUSB_CMD_SET_EYE     (0xAA)  /* set current eye */
+#define NVSTUSB_CMD_CALL_X0199  (0xBE)  /* call routine at 0x0199 */
 
 /* state of the controller */
 struct nvstusb_context {
@@ -355,6 +352,8 @@ nvstusb_deinit(
   free(ctx);
 }
 
+float nvstusb_x = 4774;
+
 /* set controller refresh rate (should be monitor refresh rate) */
 void
 nvstusb_set_rate(
@@ -364,39 +363,55 @@ nvstusb_set_rate(
   assert(ctx != 0);
   assert(ctx->handle != 0);
   assert(rate > 60);
-  
+
   /* send some magic data to device, this function is mainly black magic */
 
   /* some timing voodoo */
-  uint32_t t = NVSTUSB_CLOCK / rate;
-  uint32_t w = t * NVSTUSB_CONST_X;
-  uint32_t x = t * NVSTUSB_CONST_PHASE;
-  uint32_t y = t * NVSTUSB_CONST_DARK;
-  uint32_t z = t * NVSTUSB_CONST_Y;
-  
-  // fprintf(stderr, "nvstusb: %08x %08x %08x %08x %08x\n", t,w,x,y,z);
+  int32_t frameTime   = 1000000/rate;         /* 8.33333 ms if 120 Hz */
+  int32_t activeTime  = 2080;                 /* 2.08000 ms time each eye is on*/
+
+  int32_t w = NVSTUSB_T2_COUNT(4568.50);      /* 4.56800 ms */
+  int32_t x = NVSTUSB_T0_COUNT(4774.25);      /* 4.77425 ms */
+  int32_t y = NVSTUSB_T0_COUNT(activeTime);
+  int32_t z = NVSTUSB_T2_COUNT(frameTime);    
   
   uint8_t cmdTimings[] = { 
     NVSTUSB_CMD_WRITE,      /* write data */
     0x00,                   /* to address 0x2007 (0x2007+0x00) = ?? */
     0x18, 0x00,             /* 24 bytes follow */
 
-    w, w>>8, ~w>>16, ~w>>24,  /* 2007: ?? */ 
-    x, x>>8, ~x>>16, ~x>>24,  /* 200b: ?? */
-    y, y>>8, ~y>>16, ~y>>24,  /* 200f: ?? */
-    0x30, 0x28, 0x24, 0x22,   /* 2013: ?? */
-    0x0a, 0x08, 0x05, 0x04,   /* 2017: ?? */
-    z, z>>8,                  /* 201b: timer 2 reload value */
-    ~z>>16, ~z>>24            /* 201d: stored at 0x67, 0x68*/
+    /* original: e1 29 ff ff (-54815; -55835) */
+    w, w>>8, w>>16, w>>24,    /* 2007: ?? some timer 2 counter, 1020 is subtracted from this
+                               *       loaded at startup with:
+                               *       0x44 0xEC 0xFE 0xFF (-70588(-1020)) */ 
+    /* original: 68 b5 ff ff (-19096), 4.774 ms */
+    x, x>>8, x>>16, x>>24,    /* 200b: ?? counter saved at long at address 0x4f
+                               *       increased at timer 0 interrupt if bit 20h.1 
+                               *       is cleared, on overflow
+                               *       to 0 the code at 0x03c8 is executed.
+                               *       timer 0 will be started with this value
+                               *       by timer2 */
+
+    /* original: 81 df ff ff (-8319), 2.08 ms */
+    y, y>>8, y>>16, y>>24,    /* 200f: ?? counter saved at long at address 0x4f, 784 is added to this
+                               *       if PD1 is set, delay until turning eye off? */
+
+    /* wave forms to send via IR: */
+    0x30,                     /* 2013: 110000 PD1=0, PD2=0: left eye off  */
+    0x28,                     /* 2014: 101000 PD1=1, PD2=0: left eye on   */
+    0x24,                     /* 2015: 100100 PD1=0, PD2=1: right eye off */
+    0x22,                     /* 2016: 100010 PD1=1, PD2=1: right eye on  */
+    
+    /* ?? used when frameState is != 2, for toggling bits in Port B,
+     * values seem to have no influence on the glasses or infrared signals */
+    0x0a,                     /* 2017: 1010 */
+    0x08,                     /* 2018: 1000 */
+    0x05,                     /* 2019: 0101 */
+    0x04,                     /* 201a: 0100 */
+
+    z, z>>8, z>>16, z>>24     /* 201b: timer 2 reload value */
   }; 
   nvstusb_write_usb_bulk(ctx->handle, 2, cmdTimings, sizeof(cmdTimings));
-
-  /* timer 2 reload value in the snoop was 0x7961 = 31073
-   * if i understand the trm correctly, timer 2 is clocked by T2 pin at an unknown rate.
-   * when there is a high to low transition on T2EX, the current timer 
-   * value is saved to or loaded from RCAP2H/L depending on CPRL2 and
-   * a timer interrupt is issued.
-   * */
 
   uint8_t cmd0x1c[] = {
     NVSTUSB_CMD_WRITE,      /* write data */
@@ -406,31 +421,36 @@ nvstusb_set_rate(
     0x02, 0x00              /* ?? seems to be the start value of some 
                                counter. runs up to 6, some things happen
                                when it is lower, that will stop if when
-                               it reaches 6 */
+                               it reaches 6. could be the index to 6 byte values 
+                               at 0x17ce that are loaded into TH0*/
   };
   nvstusb_write_usb_bulk(ctx->handle, 2, cmd0x1c, sizeof(cmd0x1c));
+
+  /* wait at most 2 seconds before going into idle */
+  uint16_t timeout = rate * 2;  
     
-  uint8_t cmdRate[] = {
+  uint8_t cmdTimeout[] = {
     NVSTUSB_CMD_WRITE,      /* write data */
-    0x1e,                   /* to address 0x2025 (0x2007+0x1e) = rate? */
+    0x1e,                   /* to address 0x2025 (0x2007+0x1e) = timeout */
     0x02, 0x00,             /* 2 bytes follow */
 
-    rate<<1, rate>>7        /* rate * 2 */
-    /* if the word at 0x5f, 0x60 becomes greater than this value+20
-     * all timers are stopped. otherwise pin 2 of port d will be toggled
-     * and pin 2 of port c will be set to high if the word is larger 
-     * than 20 and the word will be increased by one. this happens
-     * every time timer2 overflows. */
+    timeout, timeout>>8     /* idle timeout (number of frames) */
   };
-  nvstusb_write_usb_bulk(ctx->handle, 2, cmdRate, sizeof(cmdRate));
+  nvstusb_write_usb_bulk(ctx->handle, 2, cmdTimeout, sizeof(cmdTimeout));
 
   uint8_t cmd0x1b[] = {
     NVSTUSB_CMD_WRITE,      /* write data */
     0x1b,                   /* to address 0x2022 (0x2007+0x1b) = ?? */
     0x01, 0x00,             /* 1 byte follows */
     
-    0x07                    /* ?? seems to be the start value of some 
-                               counter */
+    0x07                    /* ?? compared with byte at 0x29 in TD_Poll()
+                               bit 0-1: index to a table of 4 bytes at 0x17d4 (0x00,0x08,0x04,0x0C),
+                                        PB1 is set in TD_Poll() if this index is 0, cleared otherwise
+                               bit 2:   set bool21_4, start timer 1, enable ext. int. 5
+                               bit 3:   PC1 is set to the inverted value of this bit in TD_Poll()
+                               bit 4-5: index to a table of 4 bytes at 0x2a 
+                               bit 6:   restart t0 on some conditions in TD_Poll()
+                                */
   };
   nvstusb_write_usb_bulk(ctx->handle, 2, cmd0x1b, sizeof(cmd0x1b));
     
@@ -449,8 +469,9 @@ nvstusb_set_eye(
   assert(eye == nvstusb_left || eye == nvstusb_right);
 
   uint8_t buf[8] = { 
-    0xAA, (eye%2)?0xFE:0xFF,  /* set shutter state */
-    0x00, 0x00,               /* unused? */
+    NVSTUSB_CMD_SET_EYE,      /* set shutter state */
+    (eye%2)?0xFE:0xFF,        /* eye selection */
+    0x00, 0x00,               /* unused */
     r, r>>8, 0xFF, 0xFF       /* still a mystery */
   };
   nvstusb_write_usb_bulk(ctx->handle, 1, buf, 8);
@@ -470,6 +491,7 @@ nvstusb_swap(
     /* if we have the GLX_SGI_video_sync extension, we just wait
      * for vertical blanking, then issue swap. */
     unsigned int count;
+
     glXGetVideoSyncSGI(&count);
     glXWaitVideoSyncSGI(2, (count+1)%2, &count);
     nvstusb_set_eye(ctx, eye, 0);
